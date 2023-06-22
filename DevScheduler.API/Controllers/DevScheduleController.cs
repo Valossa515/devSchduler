a586e49b65c61dc619e86444b3f7cf1fc6b5a4df
Valossa515 <fe.mmo515@gmail.com>
2023-06-22 12:20:02 -0300

    Documentação Swagger

--- DevScheduler.API/Controllers/DevScheduleController.cs
+++ DevScheduler.API/Controllers/DevScheduleController.cs
@@ -1,4 +1,5 @@
-﻿using DevScheduler.API.Entities;
+﻿using Azure;
+using DevScheduler.API.Entities;
 using DevScheduler.API.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,13 +15,28 @@ namespace DevScheduler.API.Controllers
         public DevScheduleController(DevScheduleDbContent context) {
             _context = context;
         }
+
+        /// <summary>
+        /// Obter todos os eventos
+        /// </summary>
+        /// <returns>Coleção de eventos</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult GetAll() {
             var devSchedule = _context.DevSchedules.Where(d => !d.IsDeleted).ToList();
             return Ok(devSchedule);
         }
-
+        /// <summary>
+        /// Obter um evento
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <returns>Dados do evento</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult GetById(Guid id) {
             var devSchedule = _context.DevSchedules
                  .Include(de => de.Speakers)
@@ -30,15 +46,37 @@ namespace DevScheduler.API.Controllers
             }
             return Ok(devSchedule);
         }
-
+        /// <summary>
+        /// Cadastrar um evento
+        /// </summary>
+        /// <remarks>
+        /// {"title":"string","description":"string","startDate":"2023-02-27T17:59:14.141Z","endDate":"2023-02-27T17:59:14.141Z"}
+        /// </remarks>
+        /// <param name="devSchedule">Dados do evento</param>
+        /// <returns>Objeto recém-criado</returns>
+        /// <response code="201">Sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public ActionResult Post(DevSchedule devSchedule) {
             _context.DevSchedules.Add(devSchedule);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new {id = devSchedule.Id}, devSchedule);
         }
 
+        /// <summary>
+        /// Atualizar um evento
+        /// </summary>
+        /// <remarks>
+        /// {"title":"string","description":"string","startDate":"2023-02-27T17:59:14.141Z","endDate":"2023-02-27T17:59:14.141Z"}
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="input">Dados do evento</param>
+        /// <returns>Nada.</returns>
+        /// <response code="404">Não encontrado.</response>
+        /// <response code="204">Sucesso</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult Update(Guid id, DevSchedule input) {
             var devSchedule = _context.DevSchedules.SingleOrDefault(d => d.Id == id);
             if (devSchedule == null)
@@ -50,8 +88,16 @@ namespace DevScheduler.API.Controllers
             _context.SaveChanges();
             return NoContent();
         }
-
+        /// <summary>
+        /// Deletar um evento
+        /// </summary>
+        /// <param name="id">Identificador de evento</param>
+        /// <returns>Nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult Delete(Guid id) {
             var devSchedule = _context.DevSchedules.SingleOrDefault(d => d.Id == id);
             if (devSchedule == null)
@@ -62,8 +108,20 @@ namespace DevScheduler.API.Controllers
             _context.SaveChanges();
             return NoContent();
         }
-
+        /// <summary>
+        /// Cadastrar palestrante
+        /// </summary>
+        /// <remarks>
+        /// {"name":"string","talkTitle":"string","talkDescription":"string","linkedInProfile":"string"}
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="speaker">Dados do palestrante</param>
+        /// <returns>Nada</returns>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Evento não encontrado</response>
         [HttpPost("{id}/speakers")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult PostSpeaker(Guid id, DevScheduleSpeaker speaker) {
             speaker.DevScheduleId = id;
             var devSchedule = _context.DevSchedules.Any(d => d.Id == id);
--- DevScheduler.API/Program.cs
+++ DevScheduler.API/Program.cs
@@ -1,5 +1,6 @@
 using DevScheduler.API.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,23 @@ builder.Services.AddDbContext<DevScheduleDbContent>(o => o.UseSqlServer(connecti
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DevScheduler.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Felipe Martins",
+            Email = "fe_mmo@hotmail.com",
+            Url = new Uri("https://www.linkedin.com/in/felipe-martins-529a249a/")
+        }
+    });
+    var xmlFile = "DevScheduler.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
